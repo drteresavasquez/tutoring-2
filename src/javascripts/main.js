71c2a079d866066fcb121c1d61570298ff8de1b2
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-03-27 15:27:06 -0500

    moved sample

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,11 +3,13 @@
 import 'bootstrap'; // import bootstrap elements and js
 
 import '../styles/main.scss';
+import allTheThings from './sampleCode';
 
 const init = () => {
   document.querySelector('#app').innerHTML = 'Up and running';
   console.warn('YOU ARE UP AND RUNNING!');
 
+  allTheThings();
   // USE WITH FIREBASE AUTH
   // checkLoginStatus();
 };
