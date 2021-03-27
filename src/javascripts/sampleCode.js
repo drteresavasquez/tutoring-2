b9879a6dfb36dbf1fa6734c9730b67b1eea6953a
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-03-27 16:09:35 -0500

    whiteboard stuuf

--- src/javascripts/sampleCode.js
+++ src/javascripts/sampleCode.js
@@ -1,70 +1,103 @@
-// console.log('in here!')
+// // console.log('in here!')
 
-// // document.querySelector('#app').innerHTML = 'Howdy Doodie!';
-// // document.querySelector('#app').innerHTML += 'Cutie';
-// $('#app').html('Howdy Doodie!');
-// $('#app').append(`${1 + 1} is 2`);
+// // // document.querySelector('#app').innerHTML = 'Howdy Doodie!';
+// // // document.querySelector('#app').innerHTML += 'Cutie';
+// // $('#app').html('Howdy Doodie!');
+// // $('#app').append(`${1 + 1} is 2`);
 
-// // document.querySelector('#app').addEventListener('click', () => {
-// //   console.log("You clicked app!");
+// // // document.querySelector('#app').addEventListener('click', () => {
+// // //   console.log("You clicked app!");
+// // // })
+
+// // $('.app').on('click', () => {
+// //   console.log("You clicked app!")
 // // })
 
-// $('.app').on('click', () => {
-//   console.log("You clicked app!")
-// })
+// // console.log($('#app').text());
 
-// console.log($('#app').text());
+// const allTheThings = () => {
+//   const object = {
+//     name: 'Dr. T',
+//     age: 21,
+//   };
+
+//   const gimmeName = (arg) => {
+//     console.warn(object[arg]);
+//     // return object[arg];
+//   };
+
+//   gimmeName('name');
+
+//   const arrayObj = [
+//     { name: 'One', age: 5 },
+//     { name: 'Two', age: 3 },
+//     { name: 'Three', age: 6 },
+//     { name: 'Four', age: 9 },
+//     { name: 'Five', age: 7 },
+//     { name: 'Six', age: 2 },
+//   ];
+
+//   // const overSeven = [];
+
+//   // const val = arrayObj.filter((taco) => taco.age < 7)
+
+//   // const userInput = prompt('Gimme a number');
+
+//   // console.log(Number(userInput));
+
+//   const gimmeTheObjects = (number) => arrayObj.filter((taco) => taco.age < number);
+
+//   console.warn(gimmeTheObjects(Number('1')));
+
+//   // val.map((item) => item.name);
+
+//   // console.log(val);
+
+//   // arrayObj.forEach((item) => {
+//   //   if (item.age === 7) {
+//   //     overSeven.push(item);
+//   //   }
+//   // })
+
+//   // const names = overSeven.forEach((item) => {
+//   //   return item.name;
+//   // })
+
+//   // console.log(names);
+
+//   // console.log(val);
+// };
+
+// export default allTheThings;
 
 const allTheThings = () => {
-  const object = {
-    name: 'Dr. T',
-    age: 21,
-  };
+  const stringThing = '456_237_999_542_123';
 
-  const gimmeName = (arg) => {
-    console.warn(object[arg]);
-  };
+  // 1. split the string on the _
+  const strings = stringThing.split('_'); // returns an array
 
-  gimmeName('name');
+  // 2. reverse the numbers
+  let newString = '';
+  strings.reverse().forEach((item, i) => {
+    if (i === 0) {
+      newString += item;
+    } else {
+      newString += `_${item}`;
+    }
+  });
 
-  const arrayObj = [
-    { name: 'One', age: 5 },
-    { name: 'Two', age: 3 },
-    { name: 'Three', age: 6 },
-    { name: 'Four', age: 9 },
-    { name: 'Five', age: 7 },
-    { name: 'Six', age: 2 },
-  ];
+  console.warn('new String', newString);
+  // 3. put the numbers back together in a string separated by _
 
-  // const overSeven = [];
+  // reverse this string: 123_542_999_237_456
+  // console.warn(stringThing.split('_').reverse().join('_'));
 
-  // const val = arrayObj.filter((taco) => taco.age < 7)
+  const newArray = [];
+  stringThing.split('_').forEach((item) => {
+    newArray.unshift(item);
+  });
 
-  // const userInput = prompt('Gimme a number');
-
-  // console.log(Number(userInput));
-
-  const gimmeTheObjects = (number) => arrayObj.filter((taco) => taco.age < number);
-
-  console.warn(gimmeTheObjects(Number('1')));
-
-  // val.map((item) => item.name);
-
-  // console.log(val);
-
-  // arrayObj.forEach((item) => {
-  //   if (item.age === 7) {
-  //     overSeven.push(item);
-  //   }
-  // })
-
-  // const names = overSeven.forEach((item) => {
-  //   return item.name;
-  // })
-
-  // console.log(names);
-
-  // console.log(val);
+  console.warn(newArray.join('_'));
 };
 
 export default allTheThings;
